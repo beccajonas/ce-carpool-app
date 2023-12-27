90b2cc5a50224bc7da71ce499f71cb4a288d36b7
Becca Jonas <beccajonas3@gmail.com>
2023-12-27 12:04:32 -0500

    add combobox widget for ski resort selection on form

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+# import customtkinter 
 from drive import Drive
 from converter import Converter
 
@@ -6,6 +8,95 @@ class CarpoolingApp:
     def __init__(self, master):
         self.master = master
         self.master.title("Carpooling Emissions Calculator")
+        self.master.geometry("1280x720+100+50")
+        self.master.resizable(False, False)
+
+        self.input_frame = tk.Frame(self.master)
+        self.input_frame.pack()
+
+        self.create_input_widgets()
+
+    def create_input_widgets(self):
+        # labels = [
+        #     "Starting Address",
+        #     "Ski Resort:",
+        #     "Passengers (Driver Excluded)",
+        #     "Miles Per Gallon (MPG)"
+        # ]
+
+        # for idx, text in enumerate(labels):
+        #     # Create a Label widget with the text from the labels list
+        #     label = tk.Label(master=self.input_frame, text=text)
+        #     # Create an Entry widget
+        #     entry = tk.Entry(master=self.input_frame, width=25)
+        #     # Use the grid geometry manager to place the Label and
+        #     # Entry widgets in the row whose index is idx
+        #     label.grid(row=idx, column=0, sticky="e", pady=10)
+        #     entry.grid(row=idx, column=1)
+
+        self.address_label = ttk.Label(self.input_frame, text="Starting Address")
+        self.address_label.pack()
+        self.address_entry = ttk.Entry(self.input_frame)
+        self.address_entry.pack()
+        
+        self.resort_label = ttk.Label(self.input_frame, text="Ski Resort")
+        self.resort_label.pack()
+       
+        self.selected_resort = tk.StringVar()
+        self.selected_resort.set("Select a resort")  # Set the initial value
+
+        self.resort_combobox = ttk.Combobox(
+            self.input_frame, 
+            values=list(Drive.ski_resorts.keys()), 
+            height=5,
+            textvariable=self.selected_resort 
+        )
+
+        self.resort_combobox.pack()
+
+        self.passengers_label = ttk.Label(self.input_frame, text="Number of Passengers (driver excluded)")
+        self.passengers_label.pack()
+        self.passengers_entry = ttk.Entry(self.input_frame)
+        self.passengers_entry.pack()
+
+        self.mpg_label = ttk.Label(self.input_frame, text="Miles per gallon estimation")
+        self.mpg_label.pack()
+        self.mpg_entry = ttk.Entry(self.input_frame)
+        self.mpg_entry.pack()
+
+        self.calculate_button = ttk.Button(self.master, text="Calculate Emissions", command=self.display_results)
+        self.calculate_button.pack(pady=10)
+
+    def display_results(self):
+        input_address = self.address_entry.get()
+        input_resort = self.resort_combobox.get()
+        input_passengers = int(self.passengers_entry.get())
+        input_mpg = int(self.mpg_entry.get())
+        drive = Drive(input_address, input_resort, input_passengers, input_mpg)
+        emissions = drive.emissions_saved
+        distance = drive.distance
+        snow_machine = Converter.snow_machine_hours(emissions)
+        beers_brewed = Converter.beers_brewed(emissions)
+
+        # Destroy the input widgets
+        self.input_frame.destroy()
+        self.calculate_button.destroy()
+
+        # Create a new frame for displaying results
+        result_frame = ttk.Frame(self.master)
+        result_frame.pack()
+
+        result_text = f"Distance driven: {distance} miles, {emissions} lbs. of CO2 emissions"
+        result_label = ttk.Label(result_frame, text=result_text)
+        result_label.pack(pady=10)
+
+        result_text_1 = f"{snow_machine}"
+        result_label_1 = ttk.Label(result_frame, text=result_text_1)
+        result_label_1.pack(pady=10)
+
+        result_text_2 = f"{beers_brewed}"
+        result_label_2 = ttk.Label(result_frame, text=result_text_2)
+        result_label_2.pack(pady=10)
 
 # Create the main window
 root = tk.Tk()
--- drive.py
+++ drive.py
@@ -8,22 +8,23 @@ api_key = os.getenv('API_KEY')
 
 class Drive:
     ski_resorts = {
-        "Beaver Mountain": "40000 East, US-89, Garden City, UT 84028", # 1
-        "Cherry Peak": "3200 E 11000 N, Richmond, UT 84333", # 2
-        "Powder Mountain": "6965 E Powder Mountain Rd, Eden, UT 84310", # 3
-        "Nordic Valley": "3567 Nordic Valley Way, Eden, UT 84310", # 4
-        "Snowbasin": "925 E. Snowbasin Road Huntsville, Utah 84317", #5
-        "Park City": "1355 Lowell Ave, Park City, UT 84060", #6 
-        "Woodward": "3863 Kilby Rd, Park City, UT 84098", #7
-        "Brighton": "8302 S Brighton Loop Rd, Brighton, UT 84121", #8
-        "Deer Valley": "2250 Deer Valley Dr S, Park City, UT 84060", #9
-        "Solitude": "12000 Big Cottonwood Canyon Rd, Solitude, UT 84121", #10
-        "Alta": "10300 Little Cottonwood Rd, Alta, UT 84092", #11
-        "Snowbird": "9385 Snowbird Center Trail, Snowbird, UT 84092", #12
-        "Sundance": "8841 Alpine Loop Scenic Byway, Sundance, UT 84604", #13
-        "Brian Head": "329 UT-143, Brian Head, UT 84719", #14
-        "Eagle Point": "150 S W Village Cir, Beaver, UT 84713" #15
+        "Alta": "10300 Little Cottonwood Rd, Alta, UT 84092", 
+        "Beaver Mountain": "40000 East, US-89, Garden City, UT 84028", 
+        "Brian Head": "329 UT-143, Brian Head, UT 84719", 
+        "Brighton": "8302 S Brighton Loop Rd, Brighton, UT 84121", 
+        "Cherry Peak": "3200 E 11000 N, Richmond, UT 84333", 
+        "Deer Valley": "2250 Deer Valley Dr S, Park City, UT 84060", 
+        "Eagle Point": "150 S W Village Cir, Beaver, UT 84713", 
+        "Nordic Valley": "3567 Nordic Valley Way, Eden, UT 84310", 
+        "Park City": "1355 Lowell Ave, Park City, UT 84060",
+        "Powder Mountain": "6965 E Powder Mountain Rd, Eden, UT 84310", 
+        "Snowbasin": "925 E. Snowbasin Road Huntsville, Utah 84317", 
+        "Snowbird": "9385 Snowbird Center Trail, Snowbird, UT 84092",
+        "Solitude": "12000 Big Cottonwood Canyon Rd, Solitude, UT 84121", 
+        "Sundance": "8841 Alpine Loop Scenic Byway, Sundance, UT 84604", 
+        "Woodward": "3863 Kilby Rd, Park City, UT 84098" 
     }
+
 
 
     def __init__(self, start, end, passengers, mpg):
@@ -73,7 +74,7 @@ class Drive:
     @mpg.setter
     def mpg(self, mpg):
         if not isinstance(mpg, int):
-            raise Exception("Pleae enter an integer")
+            raise Exception("Please enter an integer")
         self._mpg = mpg
 
     def calculate_distance(self):
